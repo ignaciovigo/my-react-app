62107b50e3558a5003041ddc807ccc23a7e4a791
ignaciovigo <ignaciovigoe@gmail.com>
2023-01-20 10:14:26 -0300

    Formating code

--- src/components/SubNavBar.js
+++ src/components/SubNavBar.js
@@ -1,14 +1,14 @@
-import Nav from "react-bootstrap/Nav";
 import { NavLink } from "react-router-dom";
-const SubNavBar = ({ClassName}) => {
+const SubNavBar = ({ ClassName }) => {
   return (
-      <ul className={"list-subnav bg-n p-0 overflow-auto d-flex align-items-center justify-content-start justify-content-sm-center gap-2 " + ClassName}>
-        <NavLink NavLink to="/products" className="effect-link list-subnav-item text-nowrap">All</NavLink>
-        <NavLink to="/category/electronics" className="effect-link list-subnav-item text-nowrap">Electronics</NavLink>
-        <NavLink to="/category/jewelery" className="effect-link list-subnav-item text-nowrap">Jewelery</NavLink>
-        <NavLink to="/category/men's clothing" className="effect-link list-subnav-item text-nowrap">Men's Clothing</NavLink>
-        <NavLink to="/category/women's clothing" className="effect-link list-subnav-item text-nowrap">Women's Clothing</NavLink>
-      </ul>
+
+    <ul className={"list-subnav bg-n p-0 overflow-auto d-flex align-items-center justify-content-start justify-content-sm-center gap-2 sticky-top" + ClassName}>
+      <NavLink NavLink to="/products" className="effect-link list-subnav-item text-nowrap">All</NavLink>
+      <NavLink to="/category/electronics" className="effect-link list-subnav-item text-nowrap">Electronics</NavLink>
+      <NavLink to="/category/jewelery" className="effect-link list-subnav-item text-nowrap">Jewelery</NavLink>
+      <NavLink to="/category/men's clothing" className="effect-link list-subnav-item text-nowrap">Men's Clothing</NavLink>
+      <NavLink to="/category/women's clothing" className="effect-link list-subnav-item text-nowrap">Women's Clothing</NavLink>
+    </ul>
   );
 };
 
