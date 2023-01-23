d159f26431d949fd0d058b6013f1d4e1189478b1
ignaciovigo <ignaciovigoe@gmail.com>
2023-01-23 17:20:41 -0300

    [CartWidget] removido un import

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,16 +1,14 @@
 import { FaShoppingCart } from "react-icons/fa";
 import { useCart } from "../context/CartProvider";
-import Cart from "./Cart";
 const CartWidget = () => {
-  const {totalProducts} = useCart()
-
+  const { totalProducts } = useCart();
   return (
     <>
-      <button type="button" className="btn btn-nav position-relative mt-2">
-        <FaShoppingCart className="text-naranja fs-3" />
-        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-naranja">
+      <button type='button' className='btn btn-nav position-relative mt-2'>
+        <FaShoppingCart className='text-naranja fs-3' />
+        <span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-naranja'>
           {totalProducts}
-          <span className="visually-hidden">unread messages</span>
+          <span className='visually-hidden'>unread messages</span>
         </span>
       </button>
     </>
