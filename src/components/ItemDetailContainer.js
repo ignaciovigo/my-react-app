cf582a4bdebc562e6020747fd5d9ca43635e9f98
ignaciovigo <ignaciovigoe@gmail.com>
2023-01-23 18:07:06 -0300

    [Loader]Loader añadido a itemDetailContainer

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -1,17 +1,21 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ItemDetail from "./ItemDetail";
+import Loader from "./Loader";
 
 const ItemDetailContainer = () => {
   const [productDetail, setProductDetail] = useState({});
+  const [isLoading, setisLoading] = useState(false)
   const urlParams = useParams();
   
   useEffect(() => {
     const getItemDetail = async () => {
       try {
+        setisLoading(true)
         let resp = await fetch(`https://fakestoreapi.com/products/${urlParams.id}`),
           data = await resp.json();
         if (!resp.ok) throw { status: resp.status, statusText: resp.statusText };
+        setisLoading(false)
         setProductDetail(data);
       } catch (err) {
         let message =
@@ -21,7 +25,7 @@ const ItemDetailContainer = () => {
     };
     getItemDetail();
   }, [urlParams]);
-  return <ItemDetail productDetail={productDetail} />;
+  return (!isLoading) ? <ItemDetail productDetail={productDetail} /> : <Loader/>;
 };
 
 export default ItemDetailContainer;
---
+++ src/components/Loader.js
@@ -0,0 +1,55 @@
+function Loader() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      style={{ margin: "auto", background: "none" }}
+      width="150"
+      height="150"
+      display="block"
+      preserveAspectRatio="xMidYMid"
+      viewBox="0 0 100 100"
+    >
+      <circle
+        cx="50"
+        cy="50"
+        r="30"
+        fill="none"
+        stroke="#f34213"
+        strokeDasharray="47.12388980384689 47.12388980384689"
+        strokeLinecap="round"
+        strokeWidth="6"
+      >
+        <animateTransform
+          attributeName="transform"
+          dur="1.1363636363636365s"
+          keyTimes="0;1"
+          repeatCount="indefinite"
+          type="rotate"
+          values="0 50 50;360 50 50"
+        ></animateTransform>
+      </circle>
+      <circle
+        cx="50"
+        cy="50"
+        r="25"
+        fill="none"
+        stroke="#171718"
+        strokeDasharray="39.269908169872416 39.269908169872416"
+        strokeDashoffset="39.27"
+        strokeLinecap="round"
+        strokeWidth="6"
+      >
+        <animateTransform
+          attributeName="transform"
+          dur="1.1363636363636365s"
+          keyTimes="0;1"
+          repeatCount="indefinite"
+          type="rotate"
+          values="0 50 50;-360 50 50"
+        ></animateTransform>
+      </circle>
+    </svg>
+  );
+}
+
+export default Loader;
