22f8cbc38d6005c35da4dc0f97b548e8e8c6306c
ignaciovigo <ignaciovigoe@gmail.com>
2023-01-27 18:37:47 -0300

    [PurchSummary,Carousell] feat: añadidos

---
+++ src/components/Carousell.js
@@ -0,0 +1,60 @@
+import { Col, Row } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
+import Carousel from "react-bootstrap/Carousel";
+
+function Carousell() {
+  return (
+    <Carousel indicators={false} wrap={false}>
+      <Carousel.Item>
+        <Row  className="justify-content-center align-items-center fs-6">
+          <Col xs={12} className='d-flex justify-content-center'>
+            <Card style={{ width: "18rem" }} className="flex-row">
+              <Card.Img variant='top' src='holder.js/100px180' />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Row  className="justify-content-center align-items-center gap-3 fs-6">
+        <Col xs={12} className='d-flex justify-content-center'>
+            <Card style={{ width: "18rem" }} className="flex-row ">
+              <Card.Img variant='top' src='holder.js/100px180' />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Row  className="justify-content-center align-items-center gap-3 fs-6">
+        <Col xs={12} className='d-flex justify-content-center'>
+            <Card style={{ width: "18rem" }} className="flex-row">
+              <Card.Img variant='top' src='holder.js/100px180' />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default Carousell;
--- src/components/PurchaseSummary.js
+++ src/components/PurchaseSummary.js
@@ -2,20 +2,22 @@ import { Card } from "react-bootstrap";
 import { useCart } from "../context/CartProvider";
 
 const PurchaseSummary = () => {
-const {totalPrice} = useCart()
-    return (
-    <Card className="bg-n h-100">
-      <Card.Header className="text-naranja fs-3 ff-base">Order summary</Card.Header>
-      <Card.Body className="d-flex flex-column gap-2">
-        <div className="bg-negro text-gris fs-5 p-2 d-flex justify-content-between">
-        <span className="text-muted">SUBTOTAL:</span>
-        <span>$ {totalPrice().toFixed(2)}</span>
+  const { totalPrice } = useCart();
+  return (
+    <Card className='bg-n h-100'>
+      <Card.Header className='text-naranja fs-3 ff-base'>
+        Order summary
+      </Card.Header>
+      <Card.Body className='d-flex flex-column gap-2'>
+        <div className='bg-negro text-gris fs-5 p-2 d-flex justify-content-between'>
+          <span className='text-muted'>SUBTOTAL:</span>
+          <span>$ {totalPrice().toFixed(2)}</span>
         </div>
-        <div className="bg-negro text-gris fs-5 p-2 d-flex justify-content-between">
-        <span className="text-muted">TOTAL:</span>
-        <span className="text-light">$ {totalPrice().toFixed(2)}</span>
+        <div className='bg-negro text-gris fs-5 p-2 d-flex justify-content-between'>
+          <span className='text-muted'>TOTAL:</span>
+          <span className='text-light'>$ {totalPrice().toFixed(2)}</span>
         </div>
-        <button className="btn-count px-4 py-2 mt-4">Buy</button>
+        <button className='btn-count px-4 py-2 mt-4'>Buy</button>
       </Card.Body>
     </Card>
   );
