ca0055d9bf9ae68f8e3615a9dbe2abf56a0538d6
ignaciovigo <ignaciovigoe@gmail.com>
2023-02-03 18:18:11 -0300

    formulario

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import { Container } from 'react-bootstrap'
+import OrderForm from './OrderForm'
 
 const Home = () => {
   
   return (
     <Container fluid className='text-gris display-1 p-0 text-center'>
+      <div className='fs-5 m-auto row row-cols-2 justify-content-center'>
+      <OrderForm />
+      </div>
     </Container>
   )
 }
